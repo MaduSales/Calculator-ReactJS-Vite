6674f8ade506f466ff78e1d6aa791da27461d4ca
Maria Eduarda <maria.mirandadev@gmail.com>
2024-05-12 20:03:17 -0300

    repositorio calc

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import Calculadora from './components/Calculadora/Calculadora'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Calculadora/>
+    </div>
+  )
+}
